5df3f8486265bd895009531943f267d629dfee7c
BZPW <bzaniewski.gin@gmail.com>
2018-10-22 14:13:48 +0200

    Add project files.

---
+++ CSV_reader/Class1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using System.Data; 
+
+
+namespace CSV_reader
+{
+    class Class1
+    {
+
+        static void Main(string[] args)
+        {
+
+            /*var textToParse = @"id;siec_id;wojewodztwo_id;miejscowosc;lokalizacja;standard;pasmo;lac;btsid;cid1;cid2;cid3;cid4;cid5;cid6;cid7;cid8;cid9;cid0;ECID;eNBI;CLID;uwagi;LONGuke;LATIuke;aktualizacja;StationId;RNC;carrier
+1;Play;Podlaskie;Sejny;ul. Powstańców Sejneńskich - maszt własny;GSM;1800;1199;100;;;;;5;6;7;;;;;;;;23E2137;54N0616;2017-08-27;SEJ4401;;
+2;Orange;Lubelskie;Abramów;maszt Plusa;GSM;900;58607;1565;1;2;;;;;;;;0;;;;NetWorkS!;22E1809;51N2749;2016-04-10;5263;;";*/
+
+            var fileToParse = @"F:\_BZ\BTSy\btsearch.csv";
+
+            string siec_id;
+            string miejscowosc;
+            string standard;
+            string pasmo;
+            string ECID;
+            string eNBI;
+            string CLID;
+            string LONGuke;
+            string LATIuke;
+            string StationId;
+
+            using (var stringReader = new StreamReader(fileToParse))
+            {
+                using (var reader = new CsvReader(stringReader))
+                {
+                    reader.Configuration.Delimiter = ";";
+                    reader.Configuration.HasHeaderRecord = true; // If there is no header, set to false.
+                    reader.Read();
+                    reader.ReadHeader();
+
+                    while (reader.Read())
+                    {
+                        siec_id = reader.GetField("siec_id"); // Or reader.GetField(0)
+                        miejscowosc = reader.GetField("miejscowosc");
+                        standard = reader.GetField("standard");
+                        pasmo = reader.GetField("pasmo");
+                        ECID = reader.GetField("ECID");
+                        eNBI = reader.GetField("eNBI");
+                        CLID = reader.GetField("CLID");
+                        LONGuke = reader.GetField("LONGuke");
+                        LATIuke = reader.GetField("LATIuke");
+                        StationId = reader.GetField("StationId");
+
+
+                        /*Console.WriteLine($@"siec_id: {siec_id}; miejscowosc: {miejscowosc}; standard: {standard};" +
+                            $" pasmo: {pasmo}; ECID: {ECID}; eNBI: {eNBI}; CLID: {CLID}; " +
+                            $"LONGuke: {LONGuke}; LATIuke: {LATIuke}; StationId: {StationId};");*/
+
+                    }
+                    //Console.ReadKey();
+
+ 
+                }
+            }
+
+            
+
+
+        }
+
+    }
+}
---
+++ CSV_reader/Program.cs
@@ -0,0 +1,127 @@
+﻿/*using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSV_reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string filePath = @"F:\_BZ\BTSy\btsearch.csv";
+
+            // Check if file exists.
+            if (File.Exists(filePath))
+            {
+                // Read column headers from file
+                CsvReader csv = new CsvReader(File.OpenText(filePath));
+                csv.Read();
+                csv.ReadHeader();
+
+                List<string> headers = csv.Context.HeaderRecord.ToList();
+
+                // Read csv into datatable
+                System.Data.DataTable dataTable = new System.Data.DataTable();
+
+                foreach (string header in headers)
+                {
+                    dataTable.Columns.Add(new System.Data.DataColumn(header));
+                }
+
+                // Check all required columns are present
+                if (!headers.Exists(x => x == "siec_id"))
+                {
+                    throw new ArgumentException("siec_id field not present in input file.");
+                }
+                else if (!headers.Exists(x => x == "wojewodztwo_id"))
+                {
+                    throw new ArgumentException("wojewodztwo_id field not present in input file.");
+                }
+                else if (!headers.Exists(x => x == "miejscowosc"))
+                {
+                    throw new ArgumentException("miejscowosc field not present in input file.");
+                }
+                else if (!headers.Exists(x => x == "lokalizacja"))
+                {
+                    throw new ArgumentException("lokalizacja field not present in input file.");
+                }
+                else if (!headers.Exists(x => x == "standard"))
+                {
+                    throw new ArgumentException("standard field not present in input file.");
+                }
+                else if (!headers.Exists(x => x == "pasmo"))
+                {
+                    throw new ArgumentException("pasmo field not present in input file.");
+                }
+                else if (!headers.Exists(x => x == "lac"))
+                {
+                    throw new ArgumentException("lac field not present in input file.");
+                }
+                else if (!headers.Exists(x => x == "btsid"))
+                {
+                    throw new ArgumentException("btsid field not present in input file.");
+                }
+                else if (!headers.Exists(x => x == "ECID"))
+                {
+                    throw new ArgumentException("ECID field not present in input file.");
+                }
+                else if (!headers.Exists(x => x == "eNBI"))
+                {
+                    throw new ArgumentException("eNBI field not present in input file.");
+                }
+                else if (!headers.Exists(x => x == "CLID"))
+                {
+                    throw new ArgumentException("CLID field not present in input file.");
+                }
+                else if (!headers.Exists(x => x == "LONGuke"))
+                {
+                    throw new ArgumentException("LONGuke field not present in input file.");
+                }
+                else if (!headers.Exists(x => x == "LATIuke"))
+                {
+                    throw new ArgumentException("LATIuke field not present in input file.");
+                }
+                else if (!headers.Exists(x => x == "StationId"))
+                {
+                    throw new ArgumentException("StationId field not present in input file.");
+                }
+
+                // Import csv
+                while (csv.Read())
+                {
+                    System.Data.DataRow row = dataTable.NewRow();
+
+                    foreach (System.Data.DataColumn column in dataTable.Columns)
+                    {
+                        row[column.ColumnName] = csv.GetField(column.DataType, column.ColumnName);
+                    }
+
+                    dataTable.Rows.Add(row);
+                }
+               
+                /* Make sure all required columns are populated
+                if (dataTable.Select("ua_channel = '' OR ua_channel = null").Count() > 0)
+                {
+                    throw new ArgumentException("Output contains null ua_channel values.");
+                }
+
+                if (dataTable.Select("ua_device_type = '' OR ua_device_type = null").Count() > 0)
+                {
+                    throw new ArgumentException("Output contains null ua_device_type values.");
+                }
+
+                if (dataTable.Select("message_type = '' OR message_type = null OR message_type not in ('push', 'message centre')").Count() > 0)
+                {
+                    throw new ArgumentException("Output contains null or invalid message_type values.");
+                }
+            }
+
+        }
+    }
+}
+    */
