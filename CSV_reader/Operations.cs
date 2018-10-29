c4ddb7ce4db8f7f9d87e1cea814c37faad3fee26
BZPW <bzaniewski.gin@gmail.com>
2018-10-29 16:06:00 +0100

    save to file

---
+++ CSV_reader/Operations.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using CsvHelper;
+
+namespace CSV_reader
+{
+    class Operations
+    {
+
+        public static DataTable Sel_23G(string stan, string pasm, DataTable base_dt)
+        {
+
+            DataTable eksp_dt = new DataTable();
+            DataRow[] eksp_r = new DataRow[0];
+
+            eksp_r = base_dt.Select("standard = '" + stan + "' AND pasmo = '" + pasm + "' AND (LAC <> '' OR btsid <> '')");
+
+            if (eksp_r.Length > 0)
+            {
+                eksp_dt = eksp_r.CopyToDataTable();
+            }
+        
+            //print *optional
+            {
+                //string data = string.Empty;
+                //StringBuilder sb = new StringBuilder();
+                int cnt = 0;
+
+                if (null != eksp_dt && null != eksp_dt.Rows)
+                {
+                    foreach (DataRow dataRow in eksp_dt.Rows)
+                    {
+                        //foreach (var item in dataRow.ItemArray)
+                        //{
+                        //    sb.Append(item);
+                        //    sb.Append(',');
+                        //}
+                        //sb.AppendLine();
+                        cnt++;
+                    }
+                    //data = sb.ToString();
+                    //Console.WriteLine(sb);
+                    Console.WriteLine(cnt);
+                    Console.WriteLine();
+                }
+            }
+            //Console.ReadKey();
+
+            return eksp_dt;
+        }
+
+
+        public static DataTable Sel_4G(string stan, string pasm, DataTable base_dt)
+        {
+
+            DataTable eksp_dt = new DataTable();
+            DataRow[] eksp_r = new DataRow[0];
+
+            eksp_r = base_dt.Select("standard = '" + stan + "' AND pasmo = '" + pasm + "' AND (ECID <> '' OR eNBI <> '' OR CLID <> '')");
+
+            if (eksp_r.Length > 0)
+            {
+                eksp_dt = eksp_r.CopyToDataTable();
+            }
+
+            //print *optional
+            {
+                //string data = string.Empty;
+                //StringBuilder sb = new StringBuilder();
+                int cnt = 0;
+
+                if (null != eksp_dt && null != eksp_dt.Rows)
+                {
+                    foreach (DataRow dataRow in eksp_dt.Rows)
+                    {
+                        //foreach (var item in dataRow.ItemArray)
+                        //{
+                        //    sb.Append(item);
+                        //    sb.Append(',');
+                        //}
+                        //sb.AppendLine();
+                        cnt++;
+                    }
+                    //data = sb.ToString();
+                    //Console.WriteLine(sb);
+                    Console.WriteLine(cnt);
+                    Console.WriteLine();
+                }
+            }
+            //Console.ReadKey();
+
+            return eksp_dt;
+        }
+
+        public static DataTable Coords_fix(DataTable base_dt)
+        {
+            string llong, llati;
+
+            DataRow[] dr = base_dt.Select("LONGuke <> '' AND LATIuke <> ''");
+
+            for(int i=0; i<dr.Length; i++)
+            {
+                llong = dr[i]["LONGuke"].ToString().Replace('E', ',');
+                llati = dr[i]["LATIuke"].ToString().Replace('N', ',');
+
+                dr[i]["LONGuke"] = llong;
+                dr[i]["LATIuke"] = llati;
+            }
+
+            base_dt = dr.CopyToDataTable();
+
+            return base_dt;
+        }
+
+        public static void SaveToCSV(DataTable dt, string path)
+        {
+            //var origin = @"F:\_BZ\BTSy\CSV_reader.csv";
+            if (File.Exists(path))
+            {
+                try
+                {
+                    File.Delete(path);
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+            }            
+
+            using (var textWriter = File.CreateText(path))
+            using (var csv = new CsvWriter(textWriter))
+            {
+                // Write columns
+                csv.Configuration.Delimiter = ";";
+                foreach (DataColumn column in dt.Columns)
+                {
+                    csv.WriteField(column.ColumnName);
+                }
+                csv.NextRecord();
+
+                // Write row values
+                foreach (DataRow row in dt.Rows)
+                {
+                    for (var i = 0; i < dt.Columns.Count; i++)
+                    {
+                        csv.WriteField(row[i]);
+                    }
+                    csv.NextRecord();
+                }
+            }
+
+        }
+    }
+}
--- CSV_reader/Reader.cs
+++ CSV_reader/Reader.cs
@@ -11,106 +11,150 @@ namespace CSV_reader
     {
         static void Main(string[] args)
         {
-            Stopwatch stopwatch = Stopwatch.StartNew();
+            //Stopwatch stopwatch = Stopwatch.StartNew();
             //ścieżka do pliku, nazwa DT
             var path = @"F:\_BZ\BTSy\btsearch.csv";
+            DataTable dt = new DataTable("BTSearch");
 
             if (File.Exists(path))
             {
 
-                DataTable dt = new DataTable("BTSearch");
 
-                try
+                using (StreamReader sr = new StreamReader(path, Encoding.UTF8))
                 {
-
-                    using (StreamReader sr = new StreamReader(path))
+                    using (var csv = new CsvReader(sr))
                     {
-                        using (var csv = new CsvReader(sr))
+                        csv.Configuration.Delimiter = ";";
+                        csv.Configuration.HasHeaderRecord = true;
+                        csv.Read();
+                        csv.ReadHeader();
+
+                        //lista kolumn do zachowania
+                        dt.Columns.Add(new DataColumn("siec_id", typeof(String)));
+                        dt.Columns.Add(new DataColumn("miejscowosc", typeof(String)));
+                        dt.Columns.Add(new DataColumn("standard", typeof(String)));
+                        dt.Columns.Add(new DataColumn("pasmo", typeof(String)));
+                        dt.Columns.Add(new DataColumn("lac", typeof(String)));
+                        dt.Columns.Add(new DataColumn("btsid", typeof(String)));
+                        dt.Columns.Add(new DataColumn("ECID", typeof(String)));
+                        dt.Columns.Add(new DataColumn("eNBI", typeof(String)));
+                        dt.Columns.Add(new DataColumn("CLID", typeof(String)));
+                        dt.Columns.Add(new DataColumn("LONGuke", typeof(String)));
+                        dt.Columns.Add(new DataColumn("LATIuke", typeof(String)));
+                        dt.Columns.Add(new DataColumn("StationId", typeof(String)));
+
+                        //zapisanie danych do DataTable
+                        while (csv.Read())
                         {
-                            csv.Configuration.Delimiter = ";";
-                            csv.Configuration.HasHeaderRecord = true;
-                            csv.Read();
-                            csv.ReadHeader();
-
-                            //lista kolumn do zachowania
-                            dt.Columns.Add(new DataColumn("siec_id", typeof(String)));
-                            dt.Columns.Add(new DataColumn("miejscowosc", typeof(String)));
-                            dt.Columns.Add(new DataColumn("standard", typeof(String)));
-                            dt.Columns.Add(new DataColumn("pasmo", typeof(String)));
-                            dt.Columns.Add(new DataColumn("lac", typeof(String)));
-                            dt.Columns.Add(new DataColumn("btsid", typeof(String)));
-                            dt.Columns.Add(new DataColumn("ECID", typeof(String)));
-                            dt.Columns.Add(new DataColumn("eNBI", typeof(String)));
-                            dt.Columns.Add(new DataColumn("CLID", typeof(String)));
-                            dt.Columns.Add(new DataColumn("LONGuke", typeof(String)));
-                            dt.Columns.Add(new DataColumn("LATIuke", typeof(String)));
-                            dt.Columns.Add(new DataColumn("StationId", typeof(String)));
-
-                            //zapisanie danych do DataTable
-                            while (csv.Read())
+                            var row = dt.NewRow();
+                            foreach (DataColumn column in dt.Columns)
                             {
-                                var row = dt.NewRow();
-                                foreach (DataColumn column in dt.Columns)
-                                {
-                                    row[column.ColumnName] = csv.GetField(column.DataType, column.ColumnName);
-                                }
-                                dt.Rows.Add(row);
+                                row[column.ColumnName] = csv.GetField(column.DataType, column.ColumnName);
                             }
+                            dt.Rows.Add(row);
+                        }
+                        Operations.Coords_fix(dt);
 
-                            //wyświetl w konsoli
+                        //wyświetl w konsoli
+                        {
+                            //string data = string.Empty;
+                            //StringBuilder sb = new StringBuilder();
+                            int cnt = 0;
+
+                            if (null != dt && null != dt.Rows)
                             {
-                                //string data = string.Empty;
-                                //StringBuilder sb = new StringBuilder();
-                                int cnt = 0;
-
-                                if (null != dt && null != dt.Rows)
+                                foreach (DataRow dataRow in dt.Rows)
                                 {
-                                    foreach (DataRow dataRow in dt.Rows)
-                                    {
-                                        //foreach (var item in dataRow.ItemArray)
-                                        //{
-                                        //    sb.Append(item);
-                                        //    sb.Append(',');
-                                        //}
-                                        //sb.AppendLine();
-                                        cnt++;
-                                    }
-
-                                    //data = sb.ToString();
-                                    //Console.WriteLine(sb);
-                                    Console.WriteLine(cnt);
-                                    Console.WriteLine();
+                                    //foreach (var item in dataRow.ItemArray)
+                                    //{
+                                    //    sb.Append(item);
+                                    //    sb.Append(',');
+                                    //}
+                                    //sb.AppendLine();
+                                    cnt++;
                                 }
-                                //Console.ReadKey();
+
+                                //data = sb.ToString();
+                                //Console.WriteLine(sb);
+                                Console.WriteLine(cnt);
                                 Console.WriteLine();
                             }
+                            //Console.ReadKey();
+                            Console.WriteLine();
                         }
+
                     }
                 }
-                finally
-                {
-                    //!operacje na DT
-                    DataTable cdma42 = Exx.Exx_23G("CDMA", "420", dt); //brak w btsearch
-                    DataTable cdma45 = Exx.Exx_23G("CDMA", "450", dt); //brak w btsearch 
-                    DataTable gsm18 = Exx.Exx_23G("GSM", "1800", dt); //potrzebny LAC i CID
-                    DataTable gsm9 = Exx.Exx_23G("GSM", "900", dt); //potrzebny LAC i CID
-                    DataTable gsm9e = Exx.Exx_23G("E-GSM", "900", dt); //potrzebny LAC i CID -> CID = btsid --ostatnia cyfra (z jednej z kolumn cid*)
-                    DataTable lte18 = Exx.Exx_4G("LTE", "1800", dt); //identyfikacja po ENBI / CLID / ECID
-                    DataTable lte21 = Exx.Exx_4G("LTE", "2100", dt); //identyfikacja po ENBI / CLID / ECID
-                    DataTable lte26 = Exx.Exx_4G("LTE", "2600", dt); //identyfikacja po ENBI / CLID / ECID
-                    DataTable lte8 = Exx.Exx_4G("LTE", "800", dt); //identyfikacja po ENBI / CLID / ECID
-                    DataTable lte9 = Exx.Exx_4G("LTE", "900", dt); //identyfikacja po ENBI / CLID / ECID
-                    DataTable umts18 = Exx.Exx_23G("UMTS", "1800", dt); //potrzebny LAC i CID
-                    DataTable umts21 = Exx.Exx_23G("UMTS", "2100", dt); //potrzebny LAC i CID
-                    DataTable umts9 = Exx.Exx_23G("UMTS", "900", dt); //potrzebny LAC i CID
-
-                    dt.Dispose();
-                    stopwatch.Stop();
-                    Console.WriteLine("Czas: " + stopwatch.ElapsedMilliseconds);
-                    Console.ReadKey();
-
-                }
             }
+
+            //!operacje na DT
+
+
+
+            DataTable cdma42 = Operations.Sel_23G("CDMA", "420", dt); //brak w btsearch
+            DataTable cdma45 = Operations.Sel_23G("CDMA", "450", dt); //brak w btsearch 
+            DataTable gsm18 = Operations.Sel_23G("GSM", "1800", dt); //potrzebny LAC i CID
+            DataTable gsm9 = Operations.Sel_23G("GSM", "900", dt); //potrzebny LAC i CID
+            DataTable gsm9e = Operations.Sel_23G("E-GSM", "900", dt); //potrzebny LAC i CID -> CID = btsid --ostatnia cyfra (z jednej z kolumn cid*)
+            DataTable lte18 = Operations.Sel_4G("LTE", "1800", dt); //identyfikacja po ENBI / CLID / ECID
+            DataTable lte21 = Operations.Sel_4G("LTE", "2100", dt); //identyfikacja po ENBI / CLID / ECID
+            DataTable lte26 = Operations.Sel_4G("LTE", "2600", dt); //identyfikacja po ENBI / CLID / ECID
+            DataTable lte8 = Operations.Sel_4G("LTE", "800", dt); //identyfikacja po ENBI / CLID / ECID
+            DataTable lte9 = Operations.Sel_4G("LTE", "900", dt); //identyfikacja po ENBI / CLID / ECID
+            DataTable umts18 = Operations.Sel_23G("UMTS", "1800", dt); //potrzebny LAC i CID
+            DataTable umts21 = Operations.Sel_23G("UMTS", "2100", dt); //potrzebny LAC i CID
+            DataTable umts9 = Operations.Sel_23G("UMTS", "900", dt); //potrzebny LAC i CID
+
+            
+            DataTable AllBts = new DataTable();
+            AllBts.Merge(cdma42);
+            AllBts.Merge(cdma45);
+            AllBts.Merge(gsm18);
+            AllBts.Merge(gsm9);
+            AllBts.Merge(gsm9e);
+            AllBts.Merge(lte18);
+            AllBts.Merge(lte21);
+            AllBts.Merge(lte26);
+            AllBts.Merge(lte8);
+            AllBts.Merge(lte9);
+            AllBts.Merge(umts18);
+            AllBts.Merge(umts21);
+            AllBts.Merge(umts9);
+
+
+            dt.Dispose();
+            //stopwatch.Stop();
+            //Console.WriteLine("Czas: " + stopwatch.ElapsedMilliseconds);
+            //Console.ReadKey();
+
+            /*{
+                string data = string.Empty;
+                StringBuilder sb = new StringBuilder();
+                int cnt = 0;
+
+                if (null != AllBts && null != AllBts.Rows)
+                {
+                    foreach (DataRow dataRow in AllBts.Rows)
+                    {
+                        foreach (var item in dataRow.ItemArray)
+                        {
+                            sb.Append(item);
+                            sb.Append(", ");
+                        }
+                        sb.AppendLine();
+                        cnt++;
+                    }
+
+                    data = sb.ToString();
+                    Console.WriteLine(sb);
+                    Console.WriteLine(cnt);
+                    Console.WriteLine();
+                }
+            //Console.ReadKey();
+
+            }*/
+            string respath = @"F:\_BZ\BTSy\CSV_reader.csv";
+            Operations.SaveToCSV(AllBts, respath);
         }
     }
 }
